6644eb200c150758076235095d816c0aa07f988b
hftjutyir <thomas.kodehode@gmail.com>
2023-03-08 12:51:46 +0100

    added api call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import { StyledNav } from "./components/StyledNav";
 
 function App() {
--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   SearchButton,
   SearchDiv,
@@ -6,13 +7,57 @@ import {
 } from "../../components/StyledComponents";
 
 export const LandingPage = () => {
+  const [data, setData] = useState();
+  const [search, setSearch] = useState("");
+
+  const fetchData = async (callback) => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?s=${search}&apikey=ac42c6f0`
+    );
+
+    // http://www.omdbapi.com/?s=${search}&apikey=ac42c6f0
+    // http://www.omdbapi.com/?apikey=[ac42c6f0]&s=${search}
+
+    // http://www.omdbapi.com/?i=${id}&apikey=ac42c6f0
+
+    const data = await response.json();
+
+    callback(data);
+  };
+
+  const handleChange = () => {
+    setSearch(document.getElementById("ye").value);
+    console.log(search);
+  };
+
+  const handleClick = () => {
+    fetchData(setData);
+    console.log(data.Search);
+  };
+
   return (
     <div>
       <SearchDiv>
         <SearchIcon />
-        <StyledSearch placeholder="Search for a movie" />
-        <SearchButton>Search</SearchButton>
+        <StyledSearch
+          id="ye"
+          onChange={() => handleChange()}
+          placeholder="Search for a movie"
+        />
+        <SearchButton onClick={() => handleClick()}>Search</SearchButton>
       </SearchDiv>
+
+      <div>
+        {data &&
+          data.Search.map((item, index) => {
+            return (
+              <div key={index}>
+                <h1>{item.Title}</h1>
+                <img src={item.Poster} alt="" />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
