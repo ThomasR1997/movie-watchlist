855f0c8d1fb8506fa0820776a665cbe5f2868041
hftjutyir <thomas.kodehode@gmail.com>
2023-04-12 14:25:48 +0200

    added comments to watchlist page

--- src/pages/watchlistPage/WatchlistPage.js
+++ src/pages/watchlistPage/WatchlistPage.js
@@ -9,20 +9,27 @@ import {
 } from "../../components/StyledComponents";
 
 export const WatchlistPage = () => {
+  // get local storage
   const getStorage = JSON.parse(localStorage.getItem("myData"));
+
+  // set default storage state to getStorage
   const [storage, setStorage] = useState(getStorage);
 
+  // remove selected item from list
   const removeItem = (id) => {
+    // filters out selected item with id
     const newStorage = storage.filter((item) => item.id !== id);
 
+    // set storage
     setStorage(newStorage);
-    console.log("removed", storage);
   };
 
+  // saves data to local storage when storage changes
   useEffect(() => {
     localStorage.setItem(`myData`, JSON.stringify(storage));
   }, [storage]);
 
+  // Shows a message if storage is empty
   if (storage < 1) {
     return (
       <div>
